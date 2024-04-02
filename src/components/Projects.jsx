cc9d6f4b965f8d4fe5bb9012f6eaf9efa2af5c61
Andrey9019 <129278140+Andrey9019@users.noreply.github.com>
2024-04-02 22:22:25 +0300

    feat: add projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 
-import MovieSearchImg from '../img/trendingMovie.jpg';
+import MovieSearch from '../img/trendingMovie.jpg';
+import ImageSearch from '../img/searchImages.jpg';
+import CatsSearch from '../img/searchCats.jpg';
+import weatherWidget from '../img/weather.jpg';
+import taskPro from '../img/taskPro.jpg';
 
 export const Projects = () => {
   const allProjects = [
     {
-      image: MovieSearchImg,
+      image: MovieSearch,
       heading: 'Trending movies',
-      skills: 'React - React Router DOM - Axios', // Додайте список технологій/навичок, використаних у проекті
+      skills: 'React - React Router DOM - Axios',
       description:
         "This website is your guide to the world of cinema. You can easily find the latest movies, read reviews and cast lists, and quickly find the film you want by searching. For all film lovers, it's the best way to learn about and watch new releases!", // Додайте короткий опис проекту
       links: [
@@ -17,36 +21,42 @@ export const Projects = () => {
       ],
     },
     {
-      image: MovieSearchImg,
-      heading: 'Trending movies',
-      skills: 'React - React Router DOM - Axios', // Додайте список технологій/навичок, використаних у проекті
+      image: ImageSearch,
+      heading: 'Search Images',
+      skills: 'React - Axios - react-modal - react-hot-toast',
       description:
-        "This website is your guide to the world of cinema. You can easily find the latest movies, read reviews and cast lists, and quickly find the film you want by searching. For all film lovers, it's the best way to learn about and watch new releases!", // Додайте короткий опис проекту
+        'This site is an interface for searching for images using keywords. The user can enter their query in the search field and the site will display images that match this query. The search results can be simply viewed and downloaded for further use.',
       links: [
-        'https://andrey9019.github.io/goit-react-hw-05-movies/',
-        'https://github.com/Andrey9019/goit-react-hw-05-movies',
+        'https://andrey9019.github.io/goit-react-hw-04-images/',
+        'https://github.com/Andrey9019/goit-react-hw-04-images',
       ],
     },
     {
-      image: MovieSearchImg,
-      heading: 'Trending movies',
-      skills: 'React - React Router DOM - Axios', // Додайте список технологій/навичок, використаних у проекті
+      image: CatsSearch,
+      heading: 'Cat breeds',
+      skills: 'Java script - Modern Normalize - Parcel - Slim Select',
       description:
-        "This website is your guide to the world of cinema. You can easily find the latest movies, read reviews and cast lists, and quickly find the film you want by searching. For all film lovers, it's the best way to learn about and watch new releases!", // Додайте короткий опис проекту
+        'This is a website that allows you to search and learn about different cat breeds. The user can select a breed from a predefined list, and an image of the breed is displayed on the screen, along with a short description.',
       links: [
-        'https://andrey9019.github.io/goit-react-hw-05-movies/',
-        'https://github.com/Andrey9019/goit-react-hw-05-movies',
+        'https://andrey9019.github.io/goit-js-hw-10/',
+        'https://github.com/Andrey9019/goit-js-hw-10',
       ],
     },
     {
-      image: MovieSearchImg,
-      heading: 'Trending movies',
-      skills: 'React - React Router DOM - Axios', // Додайте список технологій/навичок, використаних у проекті
-      description:
-        "This website is your guide to the world of cinema. You can easily find the latest movies, read reviews and cast lists, and quickly find the film you want by searching. For all film lovers, it's the best way to learn about and watch new releases!", // Додайте короткий опис проекту
+      image: weatherWidget,
+      heading: 'weather widget',
+      skills: 'Java script - css',
+      description: 'A small widget about the weather in my home place.',
+      links: ['https://andrey9019.github.io/weather-widget/'],
+    },
+    {
+      image: taskPro,
+      heading: 'Task Pro',
+      skills: 'TypeScript - React - Tailwind ',
+      description: 'A small widget about the weather in my home place.',
       links: [
-        'https://andrey9019.github.io/goit-react-hw-05-movies/',
-        'https://github.com/Andrey9019/goit-react-hw-05-movies',
+        'https://andrey9019.github.io/weather-widget/',
+        'https://andrey9019.github.io/weather-widget/',
       ],
     },
   ];
